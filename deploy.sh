ef9be7f98728f4294c2db13c93836c18bce24f67
sarah <sarahmburbach@gmail.com>
2026-03-02 22:45:38 +0000

    fix: lldap setup

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
+#!/usr/bin/env bash
+
 SSSD_CONF=$(mktemp)
 sed "s/SSSD_BIND_PASSWORD_PLACEHOLDER/$SSSD_PASS/" auth/sssd.conf > "$SSSD_CONF"
 
 microk8s helm upgrade slurm \
   oci://ghcr.io/slinkyproject/charts/slurm \
   --values=config.yaml \
-  --set 'loginsets.slinky.enabled=true' \
-  --set-file "loginsets.slinky.rootSshAuthorizedKeys=${HOME}/.ssh/id_ed25519.pub" \
   --set-file "loginsets.slinky.sssdConf=$SSSD_CONF" \
+  --set-file "loginsets.slinky.rootSshAuthorizedKeys=${HOME}/.ssh/id_ed25519.pub" \
   --namespace=slurm
 
 rm -f "$SSSD_CONF"
 unset SSSD_PASS
 
-
-kubectl rollout restart -n slurm deployment/slurm-login-slinky
-microk8s kubectl rollout status -n slurm deployment/slurm-login-slinky
+# reset login pod
+microk8s kubectl delete pod -n slurm -l app.kubernetes.io/component=login
+# microk8s kubectl rollout restart deploy slurm-login-slinky -n slurm
