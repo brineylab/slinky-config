ee4e77a8dce089915512835bae6e3c436092b8c0
sarah <sarahmburbach@gmail.com>
2026-03-05 20:05:34 +0000

    fix: add install flag to slurm install

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 SSSD_CONF=$(mktemp)
 sed "s/SSSD_BIND_PASSWORD_PLACEHOLDER/$SSSD_PASS/" auth/sssd.conf > "$SSSD_CONF"
 
-microk8s helm upgrade slurm \
+microk8s helm upgrade --install slurm \
   oci://ghcr.io/slinkyproject/charts/slurm \
   --values=config.yaml \
   --set-file "loginsets.slinky.sssdConf=$SSSD_CONF" \
@@ -14,5 +14,4 @@ rm -f "$SSSD_CONF"
 unset SSSD_PASS
 
 # reset login pod
-microk8s kubectl delete pod -n slurm -l app.kubernetes.io/component=login
-# microk8s kubectl rollout restart deploy slurm-login-slinky -n slurm
+# microk8s kubectl delete pod -n slurm -l app.kubernetes.io/component=login
